bcd4eef553f66b233ba572060c21e6867c8d336c
zhudongzhuo <zdzserver@163.com>
2022-03-01 23:01:53 +0800

    增加解析

--- pkg/yml/yml.go
+++ pkg/yml/yml.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"gopkg.in/yaml.v3"
 	"io/ioutil"
+	"path"
+	"strconv"
 	"strings"
 )
 
@@ -12,15 +14,15 @@ func SplitByFiles(filepath []string) error {
 		if files, err := Split(path); err != nil {
 			fmt.Printf("[%s] Error: %s\n", path, err)
 		} else {
-			fmt.Printf("[%s] %s\n", path, strings.Join(files, ","))
+			fmt.Printf("[%s] %s\n", path, strings.Join(files, " "))
 		}
 	}
 	return nil
 }
 
-func Split(path string) ([]string, error) {
+func Split(filepath string) ([]string, error) {
 	files := make([]string, 0)
-	ymlByte, err := ioutil.ReadFile(path)
+	ymlByte, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		return files, err
 	}
@@ -28,6 +30,33 @@ func Split(path string) ([]string, error) {
 	if err := yaml.Unmarshal(ymlByte, &yamlObj); err != nil {
 		return files, err
 	}
-
+	ext := path.Ext(filepath)
+	filenamePrefix := strings.TrimRight(filepath, ext)
+	yamlSlice := strings.Split(string(ymlByte), "---")
+	for k, v := range yamlSlice {
+		str := strings.TrimSpace(v)
+		tag := parserTag(str)
+		if tag == "" {
+			tag = strconv.Itoa(k)
+		}
+		filename := fmt.Sprintf("%s_%s%s", filenamePrefix, tag, ext)
+		if err := ioutil.WriteFile(filename, []byte(str), 0755); err != nil {
+			return files, err
+		}
+		files = append(files, filename)
+	}
 	return files, nil
+}
+
+func parserTag(str string) string {
+	tag := ""
+	fields := strings.Fields(str)
+	key := "+parser:tag="
+	keyLen := len(key)
+	for _, v := range fields {
+		if i := strings.Index(v, key); i >= 0 {
+			tag = v[i+keyLen:]
+		}
+	}
+	return tag
 }
